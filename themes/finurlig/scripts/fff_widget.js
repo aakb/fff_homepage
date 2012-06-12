ff9ebb21e68040283fde06a8e9e78559c0e115ef
Jesper Kristensen <cableman@linuxdev.dk>
2012-06-12 16:11:25 +0200

    Added widget to the page

---
+++ themes/finurlig/scripts/fff_widget.js
@@ -0,0 +1,22 @@
+function fffWidgetCompleted() {
+  
+}
+
+var fffWidgetConfig = [];
+fffWidgetConfig.push({
+  'widget' : 'interactive',
+  'target' : '#fff-widget',
+  'loadComplet' : function () {
+    (function($) {
+      $('.fffW-widget').append('<div class="fff-quotes"></div>');
+    }(jQuery));
+  }
+});
+
+(function() {
+  "use strict";
+  
+  var fff = document.createElement('script');fff.type = 'text/javascript';fff.async = true;
+  fff.src = ('https:' === document.location.protocol ? 'https://' : 'http://') + 'service.finurligefakta.dk/widgets/fff.widget.js';
+  var s = document.getElementsByTagName('script')[0];s.parentNode.insertBefore(fff, s);
+})();
