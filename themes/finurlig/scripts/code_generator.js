569ba51e2a7e41d532ac7e8adaa085ea6961ae1a
Jesper Kristensen <cableman@linuxdev.dk>
2012-06-26 17:21:59 +0200

    Updated code generator with output

--- themes/finurlig/scripts/code_generator.js
+++ themes/finurlig/scripts/code_generator.js
@@ -13,6 +13,7 @@
      'action' : '#'
     });
 
+    // Add taget.
     generator.append($('<label />', {
       'text' : 'Identifier:'
     }).append($('<input />', {
@@ -21,6 +22,7 @@
       'value' : '#widget'
     })));
 
+    // Add type optsions.
     generator.append($('<label />', {
       'text' : 'Type:'
     }).append($('<select />', {
@@ -33,6 +35,7 @@
                 'text' : 'Slide in'
               }))));
 
+    // Add style options.
     generator.append($('<label />', {
       'text' : 'Style:'
     }).append($('<select />', {
@@ -49,14 +52,14 @@
                 'text' : 'Full'
               }))));
 
-    //<input type="radio" name="group1" value="Cheese"> Cheese
-    
+    // Add color options.
     var red = $('<label />', {
                 'text' : 'Red'
               }).append($('<input />', {
                 'type' : 'radio',
                 'name' : 'color',
-                'value' : 'red'
+                'value' : 'red',
+                'checked' :'checked'
               }));
               
     var green = $('<label />', {
@@ -68,11 +71,11 @@
               }));
               
     var blue = $('<label />', {
-                'text' : 'Red'
+                'text' : 'Blue'
               }).append($('<input />', {
                 'type' : 'radio',
                 'name' : 'color',
-                'value' : 'red'
+                'value' : 'blue'
               }));
               
     var orange = $('<label />', {
@@ -107,13 +110,47 @@
     }).click(function(e) {
       e.stopPropagation();
       e.preventDefault();
-//      alert('generate code');
+      fff_code_generator();
       return false;
     }));
 
     // Apped the form.
     target.append(generator);
   });
+
+  function fff_code_generator() {
+    var target = $('#fff-widget-generator input[name="target"]').val();
+    var type = $('#fff-widget-generator select[name="type"]').val();
+    var style = $('#fff-widget-generator select[name="style"]').val();
+    var color = $('#fff-widget-generator input[@name="color"]:checked').val();
+    
+    // Generate widget configuration.
+    var config = "var fffWidgetConfig = [];\n";
+    config += "fffWidgetConfig.push({\n";
+    config += "  'widget' : '"+type+"',\n";
+    config += "  'target' : '"+target+"',\n";
+    config += "  'style' : {\n";
+    config += "    'type' : '"+style+"',\n";
+    config += "    'color' : '"+color+"'\n";
+    config += "  },\n";
+    config += "  'tracking' : true,\n";
+    config += "  'button' : {\n";
+    config += "     'reload' : true\n";
+    config += "  },\n";
+    config += "  'event' : {\n";
+    config += "    'loadComplet' : null\n";
+    config += "  }\n";
+    config += "});\n\n";
+    
+    var load = "(function() {\n";
+    load += "  var fff = document.createElement('script'); fff.type = 'text/javascript'; fff.async = true;\n";
+    load += "  fff.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + 'service.finurligefakta.dk/widgets/fff.widget.js';\n";
+    load += "  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(fff, s);\n";
+    load += "})();"
+    
+    console.log(config + load);
+    return config;
+  }
 
 }(jQuery));
 
