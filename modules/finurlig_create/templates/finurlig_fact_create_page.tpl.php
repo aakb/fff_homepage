e46c5eacee5ee1bf0994b3a74f156f5d2150c9bb
Jesper Kristensen <cableman@linuxdev.dk>
2012-10-15 17:13:47 +0200

    Added theme function to handle create fact iframe page

---
+++ modules/finurlig_create/templates/finurlig_fact_create_page.tpl.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * @file
+ */
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML+RDFa 1.1//EN">
+<html class="no-js" lang="da" dir="ltr" version="HTML+RDFa 1.1"
+  xmlns:content="http://purl.org/rss/1.0/modules/content/"
+  xmlns:dc="http://purl.org/dc/terms/"
+  xmlns:foaf="http://xmlns.com/foaf/0.1/"
+  xmlns:og="http://ogp.me/ns#"
+  xmlns:rdfs="http://www.w3.org/2000/01/rdf-schema#"
+  xmlns:sioc="http://rdfs.org/sioc/ns#"
+  xmlns:sioct="http://rdfs.org/sioc/types#"
+  xmlns:skos="http://www.w3.org/2004/02/skos/core#"
+  xmlns:xsd="http://www.w3.org/2001/XMLSchema#">
+  <head profile="http://www.w3.org/1999/xhtml/vocab">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, minimum-scale=1, user-scalable=no" />
+    <style type="text/css" media="all">
+      @import url("<?php print $css; ?>");
+    </style>
+  </head>
+  <body>
+    <div>
+      <span class="fffw-logo"></span>
+      <?php print $form; ?>
+    </div>
+  </body>
+</html>
