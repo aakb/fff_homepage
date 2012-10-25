1ec793561047b11d52664147f9bcb78463a81e73
Jesper Kristensen <cableman@linuxdev.dk>
2012-10-25 15:06:51 +0200

    Updated code generator and demo page with add fact options

--- themes/finurlig/scripts/widget_demo.js
+++ themes/finurlig/scripts/widget_demo.js
@@ -1,8 +1,9 @@
 var fffWidgetConfig = [];
 
 (function ($) {
+  "use strict";
 
-  $(document).ready(function() { 
+  $(document).ready(function() {
     $('#generator select[name="type"]').change( function() {
       refreshWidget(false);
     });
@@ -12,23 +13,26 @@ var fffWidgetConfig = [];
     $('#generator input[name="color"]').change( function() {
       refreshWidget(false);
     });
+    $('#generator input[name="crate_link"]').change( function() {
+      refreshWidget(false);
+    });
     refreshWidget(true);
   });
 
   function refreshWidget( initial ) {
-    console.log($('#widget').html);
     if ($('#widget').html && !initial) {
       $('#widget').html('');
       var css = document.getElementsByTagName('link');
       if (css !== undefined && css.length) {
         css[css.length - 1].disabled = true;
-      } 
+      }
     }
-  
+
     var type = $('#generator select[name="type"]').val();
     var style = $('#generator select[name="style"]').val();
     var color = $('#generator input[name="color"]:checked').val();
-  
+    var create = $('#generator input[name="crate_link"]:checked').val();
+
     fffWidgetConfig = [];
     fffWidgetConfig.push({
       'widget' : type,
@@ -38,14 +42,13 @@ var fffWidgetConfig = [];
         'color' : color
       },
       'tracking' : false,
-      'button' : { 'reload' : true },
+      'button' : { 'reload' : true, 'create' : (create === undefined ? false : true) },
       'event' : { 'loadComplet' : null }
     });
     (function() {
       var fff = document.createElement('script'); fff.type = 'text/javascript'; fff.async = true;
-      fff.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + 'service.finurligefakta.dk/widgets/fff.widget.min.js';  
+      fff.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + 'service.finurligefakta.dk/widgets/fff.widget.min.js';
       var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(fff, s);
     })();
   }
-
 }(jQuery));
